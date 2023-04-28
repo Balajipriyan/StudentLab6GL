64ff4bc10c17ed541e2202c2ee15cc684420ff3b
Balajipriyan <119772870+Balajipriyan@users.noreply.github.com>
2023-04-28 15:24:12 +0530

    Add files via upload

---
+++ StudentDebate/main/Lab6_RestAPI-main/Student-Management-SpringBoot-Security-main/src/main/java/com/greatLearning/studentManagement/entity/Role.java
@@ -0,0 +1,44 @@
+package com.greatLearning.studentManagement.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+	public Role() {
+		super();
+	}
+
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+}
